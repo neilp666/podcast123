856f773f9c42bcdfc02bdd9be7ac86e2075068b8
neil patel <neil@myindianweddingapp.com>
2015-09-30 18:57:38 +0100

    designed the podcast pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def podcasts
+  end
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'podcasts' => 'pages#podcasts'
+
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.

d8719afaeb890cc636cde5d29dc5e67bc10eb4d1
neil patel <neil@myindianweddingapp.com>
2015-10-02 12:47:56 +0100

    Created Welcome Controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   get 'episode' => 'pages#episode'
 
-  root 'pages#home'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

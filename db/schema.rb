96d4f53bd3205a50b38b3a89862fe358b2cac124
neil patel <neil@myindianweddingapp.com>
2015-10-03 10:52:00 +0100

    Added attribute to devise podcasts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002120855) do
+ActiveRecord::Schema.define(version: 20151003091349) do
 
   create_table "podcasts", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20151002120855) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "itunes"
+    t.string   "stitcher"
+    t.string   "podbay"
   end
 
   add_index "podcasts", ["email"], name: "index_podcasts_on_email", unique: true

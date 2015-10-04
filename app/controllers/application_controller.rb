bdee1351fd1386ab774558c0af75c3276a720755
neil patel <neil@myindianweddingapp.com>
2015-10-04 19:20:32 +0100

    Styled sign up account and forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :title)}
-  end
-  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password,
       :title, :description, :itunes, :stitcher, :podbay)}
   end

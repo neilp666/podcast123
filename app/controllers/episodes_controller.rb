fc06adc97ec5ffd0e8fdb7edce73e17704452b5c
neil patel <neil@myindianweddingapp.com>
2015-10-06 11:41:02 +0100

    Added mp3 field

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,7 +2,7 @@ class EpisodesController < ApplicationController
   before_action :authenticate_podcast!, except: [:show]
   before_filter :require_permission
   before_action :find_podcast
-  before_action :find_episode, only: [:show, :edit, :update, :destory]
+  before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
   def new
     @episode = @podcast.episodes.new
@@ -40,7 +40,7 @@ class EpisodesController < ApplicationController
   private 
 
   def episode_params
-    params.require(:episode).permit(:title, :description, :episode_thumbnail)
+    params.require(:episode).permit(:title, :description, :episode_thumbnail, :mp3)
   end
 
   def find_podcast
